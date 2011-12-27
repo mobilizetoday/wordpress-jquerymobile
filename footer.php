4a3a60beb044adec90cea0bab35b41e12f28f8a8
mobilizetoday <github@mobilizetoday.com>
2011-12-27 13:22:18 +0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 			<div data-role="footer" class="ui-bar"<?php jqmobile_ui('footer');?>>
 				<div class="logos">
 					<a href="http://wordpress.org/" class="icon icon-custom-wp">Wordpress</a>
-					<a href="http://http://jquerymobile.com/" class="icon icon-custom-jqm">jQueryMobile</a>
+					<a href="http://jquerymobile.com/" class="icon icon-custom-jqm">jQueryMobile</a>
 					<a href="http://www.mobilizetoday.com/" class="icon icon-custom-mt">MobilizeToday</a>
 				</div>
 				<a href="#jqm-home"  data-role="button" data-icon="arrow-u" id="button-up">Up</a>
